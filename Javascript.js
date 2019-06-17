6bf5e1b2b8f472120c1120fc9d8b52107f01d7c6
Jasmyn Newton <jasmyn.n.newton@gmail.com>
2019-06-16 23:21:48 -0400

    Finished Variable Referencing

--- Javascript.js
+++ Javascript.js
@@ -1,10 +1,13 @@
-var inventory = [];
+var inventory = ["Ale", "Mead"];
 
 var goldBalance = 30;
 
 var hopsStock = 0;
 var yeastStock = 0;
 var honeyStock = 0;
+
+var aleStock = 0;
+var meadStock = 0;
 
 var hopsValue = 1.00;
 var yeastValue = 1.50;
@@ -18,6 +21,7 @@ var customer2order = "Mead";
 var customer3order = "None";
 var customer4order = "None";
 
+// this runs the displayVars function when page is loaded
 document.body.onload = function() {displayVarsToHTML()};
 
 function displayVarsToHTML() {
@@ -25,4 +29,19 @@ function displayVarsToHTML() {
     document.getElementById("customer2order").innerHTML = customer2order;
     document.getElementById("customer3order").innerHTML = customer3order;
     document.getElementById("customer4order").innerHTML = customer4order;
-}
+ 	document.getElementById("hopsValue").innerHTML = Number(hopsValue).toFixed(2);
+ 	document.getElementById("yeastValue").innerHTML = Number(yeastValue).toFixed(2);
+ 	document.getElementById("honeyValue").innerHTML = Number(honeyValue).toFixed(2); 
+ 	document.getElementById("aleStock").innerHTML = aleStock;
+ 	document.getElementById("meadStock").innerHTML = meadStock;
+	document.getElementById("hopsStock").innerHTML = hopsStock;
+    document.getElementById("yeastStock").innerHTML = yeastStock;
+    document.getElementById("honeyStock").innerHTML = honeyStock;
+    document.getElementById("goldBalance").innerHTML = Number(goldBalance).toFixed(2);
+    document.getElementById("inventory").innerHTML = inventory.toString();
+}	
+
+// List of Functions To Make
+// buy stock
+// put drink in inventory
+// serve drink to customer
