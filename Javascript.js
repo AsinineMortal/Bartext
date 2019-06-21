60b177c3531aaad75720a6628c4dffd24ec2ce7b
Jasmyn Newton <jasmyn.n.newton@gmail.com>
2019-06-21 18:14:34 -0400

    Added some notes and ideas

--- Javascript.js
+++ Javascript.js
@@ -1,11 +1,20 @@
 // FUNCTIONALITY ---------------------------------------
 // Customers generate new orders over time DONE ***
 // What to do when you try and serve a customer that has no order DONE ***
-// You cannot go in negative Gold Balance
+// You cannot go in negative Gold Balance DONE ***
 // You cannot go in negative stock DONE ***
 // You cannot go in negative drink stock DONE ***
 // You earn money from customer sales DONE ***
 // Figure out the Inventory vs. Stock thing DONE ***
+
+// CONTENT ---------------------------------------------
+// Build out recipe list/drink menu
+// Add food/complex orders
+// Day/night cycle
+// Bar Upgrades
+// Daily or Montly Rent
+
+// SUSTAINABILITY / SCALE
 // Write function to create button for each ingredient
 // Write function to create button for each known drink
 // Write function to create button for current customers
@@ -18,6 +27,7 @@
 
 // POLISH ----------------------------------------------
 // Name Randomization
+// Customers generate after a certain period of time
 
 // FUNNY ADDITIONS -------------------------------------
 // Kick out loitering customers
@@ -32,7 +42,17 @@ var honey = {name: "honey", stock: 0, value: 2.00};
 var ale = {name: "ale", stock: 0, price: 5.00, ingredients: [yeast, hops]};
 var mead = {name: "mead", stock: 0, price: 6.50, ingredients: [yeast, honey]};
 
-var drinkList = [ale, mead];
+var knownDrinks = [ale, mead];
+var knownIngredients = [hops, yeast, honey];
+
+var customerNameList = {
+	first: [
+		Alleta, Amund, Arvid, Bjorn, Bo, Canute, Carolus, Carson, Dag, Dagna, Dahlia, Davin, Delling, Denholm, Dyre, Egil, Eir, Elvis, Eric, Erica, Evonne, Filip, Fortinbras, Frey, Freya, Girls, Gudrun, Gunda, Haakon, Halle, Hannes, Idonea, Igor, Ingmar, Ingrid, Ingvar, Jarl, Jesen, Kaj, Karena, Karr, Kelda, Kelsey, Kelsey, Kerr, Kettil, Kiersten, Knute, Kolby, Krystyn, Lang, Lars, Larsen, Linnea, Lis, Lisolette, Litzy, Loki, Ludvig, Lundy, Lunt, Malin, Marny, Mats, Mia, Nels, Niels, Nissa, Norb, Odell, Odo, Olan, Pal, Quenby, Quinby, Quinby, Rad, Ran, Ranveig, Rinda, Runa, Rune, Sanna, Savea, Selma, Signa, Signild, Sigrun, Skjøld, Snorre, Snøfrid, Sonja, Sorley, Steinar, Stieran, Svea, Sven, Synnove, Sølve, Thiassi, Thoralf, Thorborg, Thorin, Thorstein, Thurid, Thyrrni, Toril, Toste, Toyah, Tryggvi, Tyra, Ula, Ulffr, Ulick, Ull, Ulrik, Ulster, Ultar, Unn, Vali, Valkyrie, Vanja, Vannevar, Vanslow, Vidar, Vidkun, Vigdis, Viggo, Viljalmr, Viljo, Viola, Von, Waage, Wray, Ylwa, Zainab, Zvi
+		],
+	last: [
+		Aaberg, Aland, Alf, Alfson, Ander, Askelson, Aven, Axel, Backe, Bakken, Becher, Beck, Bell, Bengtsson, Benson, Berg, Bergh, Bernt, Bi, Bielke, Bjerk, Bjerke, Blom, Bonde, Borg, Borgerson, Borgeson, Brand, Bro, Bugge, Bugge, Bunderson, Carlson, Christeson, Dal, Daman, Dolph, Edman, Elison, Ellingboe, Emanuelson, Ericson, Erling, Eskelson, Eskildsen, Estenson, Fagerberg, Falk, Falkenberg, Forberg, Fredrikson, Friberg, Friis, Frydenlund, Gangestad, Gulbrandsen, Gulbrandson, Gulbranson, Gustason, Gustavson, Guttormson, Haldorson, Halpain, Halvorson, Haralson, Hartvigsen, Hatlen, Haugrud, Haugstad, Helgason, Helvig, Heskin, Hexum, Hillesland, Hjelmstad, Hjorth, Hohlt, Holmen, Jostad, Junge, Kaase, Karsten, Klingenberg, Knudtson, Kolbeck, Kolsrud, Krog, Krogh, Krum, Kvalheim, Leif, Lind, Lorenson, Lund, Mathieson, Mathison, Matsen, Ness, Nord, Nyberg, Nybo, Nyborg, Nygaard, Nygard, Nyhus, Nyland, Nylund, Ohlson, Olafson, Oscar, Peerson, Pihl, Riis, Ring, Risberg, Salverson, Sandberg, Sandoe, Sigmond, Skau, Soelberg, Steenberg, Steensen, Stinar, Thomason, Thor, Thorp, Thostenson, Toft, Tofte, Torgrimson, Torp, Tostenson, Tranum, Truelson, Trulson, Trygg, Vagle, Vang, Westberg, Westergaard, Westergard
+		]
+	};
 
 var customer1 = {id: "customer1", name: "Ingrid", order: ale};
 var customer2 = {id: "customer2", name: "Hans", order: mead};
@@ -145,9 +165,9 @@ function serveCustomer(customer) {
 }
 
 function generateNewOrder(customer) {
-		var numberOfDrinksModifier = drinkList.length;
+		var numberOfDrinksModifier = knownDrinks.length;
 		var random = Math.floor(Math.random() * numberOfDrinksModifier);
-		customer.order = drinkList[random];
+		customer.order = knownDrinks[random];
 		
 		document.getElementById(customer.id + "Order").innerHTML = customer.order.name;
 	}
